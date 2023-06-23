89a209a682c951d21aa379e4ffa40b90b67ca0d7
Steven Chalem <steven.chalem@semanticarts.com>
2023-06-23 12:31:02 -0700

    added fingerprints, better IRI minting

--- src/congr copy 2.py
+++ src/congr copy 2.py
@@ -1,20 +1,11 @@
 import os
 from datetime import datetime
-import hashlib
 from rdflib import Graph, Literal, Namespace, RDF, URIRef, XSD
 from urllib.parse import quote
 import mimetypes
 
 gist = Namespace("https://w3id.org/semanticarts/gist/")
 dat = Namespace("http://data.semanticarts.com/congr/") # Namespace for our file metadata
-
-# Create a function to generate a SHA256 hash
-def generate_hash(input_string, length=None):
-    hash_result = hashlib.sha256(input_string.encode()).hexdigest()
-    if length is not None:
-        return hash_result[:length]
-    else:
-        return hash_result
 
 def generate_file_metadata(dir_path):
     # Create a graph
@@ -23,6 +14,7 @@ def generate_file_metadata(dir_path):
     g.bind('dat', dat)  # Associate the prefix 'dat' with the dat namespace
 
     # Use os.walk() to traverse entire directory tree and identify directories and files
+    # use rdflib to create nodes for each directory and file
     for root, dirs, files in os.walk(dir_path):
         # Create a node for the directory
         dir_node = URIRef(dat + quote(root, safe=''))
@@ -40,11 +32,8 @@ def generate_file_metadata(dir_path):
             file_path = os.path.join(root, filename)
 
             if os.path.isfile(file_path):
-                # Generate a short hash for the IRI and a longer one for the property
-                short_hash = generate_hash(file_path + str(os.path.getmtime(file_path)), length=10)
-                long_hash = generate_hash(file_path + str(os.path.getmtime(file_path)))
-                # Create a node for the file using the new format, with underscore between file name and hash
-                file_node = URIRef(dat + "_Content_" + quote(filename, safe='') + "_" + short_hash)
+                # Create a node for the file
+                file_node = URIRef(dat + quote(filename, safe=''))
 
                 # Add triples for the file node and its properties
                 g.add((file_node, RDF.type, gist.Content))
@@ -53,7 +42,6 @@ def generate_file_metadata(dir_path):
                 g.add((file_node, dat.creation_time, Literal(datetime.fromtimestamp(os.path.getctime(file_path)).isoformat(), datatype=XSD.dateTime)))
                 g.add((file_node, dat.modification_time, Literal(datetime.fromtimestamp(os.path.getmtime(file_path)).isoformat(), datatype=XSD.dateTime)))
                 g.add((file_node, dat.directory, Literal(os.path.dirname(file_path), datatype=XSD.string)))
-                g.add((file_node, dat.fingerprint, Literal(long_hash, datatype=XSD.string)))  # Add the long hash as a property
 
                 # Determine and add the MIME type
                 mime_type = mimetypes.guess_type(file_path)[0]
