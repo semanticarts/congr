5a59ddd86ab1b84c630204f5cdc0b192ea342a84
Steven Chalem <steven.chalem@semanticarts.com>
2023-06-27 17:20:04 -0700

    file cleanup

--- congr-prev.py
+++ congr-prev.py
@@ -11,7 +11,7 @@ congr = Namespace("https://ontologies.semanticarts.com/congr/")
 congr3 = Namespace("https://data.semanticarts.com/congr/")
 
 def spacetime_hash(input_string):
-    return hashlib.sha256(str(input_string).encode()).hexdigest()[:10]
+    return hashlib.sha256(input_string.encode()).hexdigest()[:10]
 
 def content_hash(file_path):
     hasher = hashlib.sha256()
@@ -24,47 +24,6 @@ def content_hash(file_path):
         return None
     return hasher.hexdigest()
 
-def generate_node(name, hash):
-    return URIRef(congr3 + "_Content_" + quote(name, safe='') + "_" + hash)
-
-def handle_directory(root, g, starting_dir_node):
-    dir_hash = spacetime_hash(root + str(os.path.getctime(root)))
-    dir_node = generate_node(os.path.basename(root), dir_hash)
-    g.add((dir_node, RDF.type, gist.Collection))
-    g.add((dir_node, gist.name, Literal(os.path.basename(root), datatype=XSD.string)))
-    g.add((dir_node, congr.pathString, Literal(root, datatype=XSD.anyURI)))
-    g.add((dir_node, gist.isMemberOf, starting_dir_node))
-    return dir_node
-
-def handle_file(file_path, filename, g, create_fingerprints, dir_node=None):
-    file_location_hash = spacetime_hash(file_path + str(os.path.getctime(file_path)))
-    file_node = generate_node(filename, file_location_hash)
-
-    g.add((file_node, RDF.type, gist.Content))
-    g.add((file_node, gist.name, Literal(filename, datatype=XSD.string)))
-
-    size_node = URIRef(congr3 + "_InformationQuantity_" + quote(filename, safe='') + "_" + file_location_hash)
-    g.add((size_node, gist.hasUnitOfMeasure, XSD.byte))
-    g.add((size_node, gist.hasValue, Literal(os.path.getsize(file_path), datatype=XSD.integer)))
-    g.add((file_node, gist.hasMagnitude, size_node))
-
-    g.add((file_node, congr.createDateTime, Literal(datetime.fromtimestamp(os.path.getctime(file_path)).isoformat(), datatype=XSD.dateTime)))
-    g.add((file_node, congr.modifyDateTime, Literal(datetime.fromtimestamp(os.path.getmtime(file_path)).isoformat(), datatype=XSD.dateTime)))
-
-    mime_type = mimetypes.guess_type(file_path)[0]
-    if mime_type:
-        mime_type_node = URIRef(gist + "_MediaType_" + quote(mime_type.replace('/', ''), safe=''))
-        g.add((mime_type_node, RDF.type, gist.MediaType))
-        g.add((file_node, gist.hasMediaType, mime_type_node))
-
-    if create_fingerprints:
-        fingerprint = content_hash(file_path)
-        if fingerprint is not None:
-            g.add((file_node, congr.fingerprint, Literal(fingerprint, datatype=XSD.string)))
-
-    if dir_node:
-        g.add((file_node, gist.isMemberOf, dir_node))
-
 def generate_file_metadata(starting_dir_path, include_files, create_fingerprints, output_file='congr-output.ttl'):
     g = Graph()
     g.bind('gist', gist)
@@ -72,21 +31,50 @@ def generate_file_metadata(starting_dir_path, include_files, create_fingerprints
     g.bind('congr3', congr3)
 
     starting_dir_hash = spacetime_hash(starting_dir_path + str(os.path.getmtime(starting_dir_path)))
-    starting_dir_node = generate_node(os.path.basename(starting_dir_path), starting_dir_hash)
+    starting_dir_node = URIRef(congr3 + "_Directory_" + quote(os.path.basename(starting_dir_path), safe='') + "_" + starting_dir_hash)
     g.add((starting_dir_node, RDF.type, gist.Collection))
     g.add((starting_dir_node, gist.name, Literal(os.path.basename(starting_dir_path), datatype=XSD.string)))
     g.add((starting_dir_node, congr.pathString, Literal(starting_dir_path, datatype=XSD.anyURI)))
 
     for root, dirs, files in os.walk(starting_dir_path):
-        dir_node = None
-        if root != starting_dir_path:
-            dir_node = handle_directory(root, g, starting_dir_node)
-        
         for filename in files:
             file_path = os.path.join(root, filename)
+
             if os.path.isfile(file_path) and include_files:
-                handle_file(file_path, filename, g, create_fingerprints, dir_node)
-    
+                file_location_hash = spacetime_hash(file_path + str(os.path.getctime(file_path)))
+                file_node = URIRef(congr3 + "_Content_" + quote(filename, safe='') + "_" + file_location_hash)
+
+                g.add((file_node, RDF.type, gist.Content))
+                g.add((file_node, gist.name, Literal(filename, datatype=XSD.string)))
+
+                size_node = URIRef(congr3 + "_InformationQuantity_" + quote(filename, safe='') + "_" + file_location_hash)
+                g.add((size_node, gist.hasUnitOfMeasure, XSD.byte))
+                g.add((size_node, gist.hasValue, Literal(os.path.getsize(file_path), datatype=XSD.integer)))
+                g.add((file_node, gist.hasMagnitude, size_node))
+
+                g.add((file_node, congr.createDateTime, Literal(datetime.fromtimestamp(os.path.getctime(file_path)).isoformat(), datatype=XSD.dateTime)))
+                g.add((file_node, congr.modifyDateTime, Literal(datetime.fromtimestamp(os.path.getmtime(file_path)).isoformat(), datatype=XSD.dateTime)))
+
+                mime_type = mimetypes.guess_type(file_path)[0]
+                if mime_type:
+                    mime_type_node = URIRef(gist + "_MediaType_" + quote(mime_type.replace('/', ''), safe=''))
+                    g.add((mime_type_node, RDF.type, gist.MediaType))
+                    g.add((file_node, gist.hasMediaType, mime_type_node))
+
+                if create_fingerprints:
+                    fingerprint = content_hash(file_path)
+                    g.add((file_node, congr.fingerprint, Literal(fingerprint, datatype=XSD.string)))
+
+                if root != starting_dir_path:
+                    dir_hash = spacetime_hash(root + str(os.path.getctime(root)))
+                    dir_node = URIRef(congr3 + "_Directory_" + quote(os.path.basename(root), safe='') + "_" + dir_hash)
+                    g.add((dir_node, RDF.type, gist.Collection))
+                    g.add((dir_node, gist.name, Literal(os.path.basename(root), datatype=XSD.string)))
+                    g.add((dir_node, congr.pathString, Literal(root, datatype=XSD.anyURI)))
+                    g.add((dir_node, gist.isMemberOf, starting_dir_node))
+
+                    g.add((file_node, gist.isMemberOf, dir_node))
+
     g.serialize(format='turtle', destination=output_file)
 
 def main():
